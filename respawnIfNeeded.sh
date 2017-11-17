b946143f973eb1c423bf8c044307f55e665c8949
Hrafnkell Eiriksson <hrafnkelle@gmail.com>
2017-11-17 23:14:16 +0000

    more typos

--- respawnIfNeeded.sh
+++ respawnIfNeeded.sh
@@ -5,7 +5,7 @@ if ! daemon --name=day --running && ! daemon --name=night --running; then
 	echo Starting day
 	exit 
 fi
-if (! /home/pi/cam/isNighttime.py) && daemon --name=night --running; then
+if (! ~/mengivakt/isNighttime.py) && daemon --name=night --running; then
 	daemon --name=night --stop
 	sleep 1
 	daemon --name=day -- raspistill -mm average -ISO 100 -n -rot 180 -w 640 -h 480 -v -drc high -t 0 -ts -o /var/ramdisk/pic%d.jpg --latest /var/ramdisk/pic.jpg -a 12
@@ -13,7 +13,7 @@ if (! /home/pi/cam/isNighttime.py) && daemon --name=night --running; then
 	exit
 fi
 
-if /home/pi/cam/isNighttime.py && daemon --name=day --running; then
+if ~/mengivakt/isNighttime.py && daemon --name=day --running; then
 	daemon --name=day --stop
 	sleep 1
 	daemon --name=night -- raspistill -ev 4 -awb off -awbg 1.0,2.2 -ex verylong -mm average -ISO 16000 -ss 80000000 -n -rot 180 -w 640 -h 480 -v -drc high -t 0 -ts -o /var/ramdisk/pic%d.jpg --latest /var/ramdisk/pic.jpg -a 12
