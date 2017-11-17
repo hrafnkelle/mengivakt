6340fbfed560c53a9e5f14e91fa15416324bc358
Hrafnkell Eiriksson <hrafnkelle@gmail.com>
2017-11-17 23:10:07 +0000

    fixing a typo

--- isNighttime.py
+++ isNighttime.py
@@ -4,7 +4,7 @@ import math
 import uploadConfig as cfg
 sun = ephem.Sun()
 midengi = ephem.Observer()
-midengi.lat, midengi.lon = cfg.midelngi_lat, cfg.midengi_lon
+midengi.lat, midengi.lon = cfg.midengi_lat, cfg.midengi_lon
 midengi.date = ephem.now()
 sun.compute(midengi)
 print(math.degrees(float(sun.alt)))
